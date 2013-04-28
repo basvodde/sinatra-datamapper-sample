008ee71dd0259cc8841703cb067e3422050750b8
roofimon <roofimon@gmail.com>
2013-04-29 00:32:47 +0700

    Change to REST

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'json'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/dev.db")
 
@@ -10,6 +11,13 @@ class Article
   property :id, Serial
   property :title, String
   property :content, Text
+
+  def self.parse_json(body)
+    json = JSON.parse(body)
+    ret = { :title => json['article']['title'], :content => json['article']['content'] }
+    ret 
+  end
+
 end
 
 DataMapper.finalize.auto_upgrade!
@@ -23,43 +31,15 @@ get '/articles' do
   erb :'articles/index'
 end
 
-get '/articles/new' do
-  erb :'articles/new'
-end
-
-get '/articles/:id' do |id|
-  @article = Article.get!(id)
-  erb :'articles/show'
-end
-
-get '/articles/:id/edit' do |id|
-  @article = Article.get!(id)
-  erb :'articles/edit'
-end
-
 post '/articles' do
-  article = Article.new(params[:article])
-  
+  data = Article.parse_json(request.body.read)
+  article = Article.new(data)  
   if article.save
-    redirect '/articles'
+    status 201
   else
-    redirect '/articles/new'
+    status 500
   end
 end
 
-put '/articles/:id' do |id|
-  article = Article.get!(id)
-  success = article.update!(params[:article])
-  
-  if success
-    redirect "/articles/#{id}"
-  else
-    redirect "/articles/#{id}/edit"
-  end
-end
+#curl -v -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '{"widget":{"title":"this is title","content":"some_name"}}' http://localhost:4567/articles
 
-delete '/articles/:id' do |id|
-  article = Article.get!(id)
-  article.destroy!
-  redirect "/articles"
-end
